871b257a2fdc161a365004ed633e5722ebdf7768
deniz <>
2021-01-01 15:43:32 +0300

    update custom dialog title and message

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -54,8 +54,12 @@ class _HomePageState extends State<HomePage> {
       await showDialog<bool>(
         context: context,
         builder: (context) => AlertDialog(
-          title: const Text('Explain Title'),
-          content: const Text('Explain Description'),
+          title: const Text('Dear User'),
+          content: const Text(
+            'We care about your privacy and data security. We keep this app free by showing ads. '
+            'Can we continue to use your data to tailor ads for you?\n\nYou can change your choice anytime in the app settings. '
+            'Our partners will collect data and use a unique identifier on your device to show you ads.'
+          ),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context, false),
